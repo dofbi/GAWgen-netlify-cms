ad3beaed30914993ce1902c14778d8ee339ee802
Mamadou Diagne <genova@dofbi.com>
2022-04-03 02:10:20 +0000

    ✨ feat(List Widget): Variable Types

--- public/admin/js/workflowpreview.js
+++ public/admin/js/workflowpreview.js
@@ -2,41 +2,18 @@ const WorkflowPreview = createClass({
   render: function () {
     const entry = this.props.entry;
     const name = entry.getIn(['data', 'name']);
-    const on = entry.getIn(['data', 'on']);
+    const on = entry.getIn(['data', 'on']).toJS();
     const jobs = entry.getIn(['data', 'jobs']);
-    
+
+    code = `nam: ${name}\n\n`;
+    code += `on:\n\t`;
+    code += on.map( (key,value) => (
+      `${key}: ${value}`
+    ) )
+
     return h('div', {},
       h('h1', {}, `${name} - Preview`),
-      h('table', {},
-        h('tbody', {}
-          , h('tr', {}
-            , h('th', {}, 'Name')
-            , h('td', {}, name)
-          )
-          , h('tr', {}
-            , h('th', {}, 'On')
-            , h('td', null
-              , h('ul',null, on.map((event, index) => (
-                h('ul',null, event ? event.map((item, i) => (
-                  h('ul', {}, item ? item.map((element, i)=> (
-                    h('li', {}, element)
-                  )) : '')
-              )) : '')
-            ) )
-          ))
-          )
-          , h('tr', {}
-            , h('th', {}, 'Jobs')
-            , h('td', null
-              , h('ul', null, jobs.map((event, index) => (
-                h('ul', null, event ? event.map((item, i) => (
-                  h('li', {}, item )
-                )) : '')
-              ))
-            ))
-          )
-        )
-      )
+      h('textarea',{}, code)
     );
   }
 });
